c37238dee1fc5cc93c0295ad54f30aee41fbffaa
Marco Carta <Marco Carta@192.168.1.45>
2022-08-25 09:58:19 +0200

    First Commit

---
+++ spring_openshift/src/test/java/com/openshift/demo/SpringOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
