5e01fbb0835b8d5fbec2deafe75881cfea63b470
Marco Carta <Marco Carta@192.168.1.45>
2022-08-25 12:01:42 +0200

    First Commit

--- spring_openshift/src/main/java/com/openshift/demo/SpringOpenshiftApplication.java
+++ spring_openshift/src/main/java/com/openshift/demo/SpringOpenshiftApplication.java
@@ -2,10 +2,24 @@ package com.openshift.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringOpenshiftApplication {
 
+	@GetMapping("/")
+	public String welcome() {
+		return "Benvenuto a casa Carta";		
+	}
+	
+	@GetMapping("/{input}")
+	public String complimenti(@PathVariable String input) {
+		return "Benvenuto a casa Carta caro " + input;		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOpenshiftApplication.class, args);
 	}
